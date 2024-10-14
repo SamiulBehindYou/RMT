dc39b9ce5e5a79c4243b06ef99e80ae59fabc136
SAMIUL ISLAM <124153734+SamiulBehindYou@users.noreply.github.com>
2024-10-14 19:38:40 +0600

    404 error

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="author" content="Untree.co">
+  <link rel="shortcut icon" href="favicon.png">
+
+  <meta name="description" content="" />
+  <meta name="keywords" content="bootstrap, bootstrap4" />
+
+		<!-- Bootstrap CSS -->
+		<link href="{{ asset('frontend') }}/css/bootstrap.min.css" rel="stylesheet">
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+		<link href="{{ asset('frontend') }}/css/tiny-slider.css" rel="stylesheet">
+		<link href="{{ asset('frontend') }}/css/style.css" rel="stylesheet">
+		<title>RMT.com</title>
+	</head>
+
+	<body>
+
+
+
+
+	<div class="untree_co-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center pt-5">
+                <span class="display-3 thankyou-icon text-primary">
+                    {{-- <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cart-check mb-5" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M11.354 5.646a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L8 8.293l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+                    <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+                    </svg> --}}
+                    <svg width="1em" height="1em" style="overflow:visible;enable-background:new 0 0 32 32" viewBox="0 0 32 32" width="32" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><g id="Error_1_"><g id="Error"><circle cx="16" cy="16" id="BG" r="16" style="fill:#D72828;"/><path d="M14.5,25h3v-3h-3V25z M14.5,6v13h3V6H14.5z" id="Exclamatory_x5F_Sign" style="fill:#E6E6E6;"/></g></g></g></svg>
+                </span>
+                <h2 class="display-3 text-black">404!</h2>
+                <h2 class="display-3">Page Not found!</h2>
+                <p class="lead mb-5"></p>
+                <p><a href="{{ route('index') }}" class="btn btn-sm btn-outline-black">Back to shop</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+		<script src="{{ asset('frontend') }}/js/bootstrap.bundle.min.js"></script>
+		<script src="{{ asset('frontend') }}/js/tiny-slider.js"></script>
+		<script src="{{ asset('frontend') }}/js/custom.js"></script>
+	</body>
+
+</html>
