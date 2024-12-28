d410dca7392a40596135178fe04adc9c4c514eed
SAMIUL ISLAM <124153734+SamiulBehindYou@users.noreply.github.com>
2024-12-28 12:03:10 +0600

    config

--- config/app.php
+++ config/app.php
@@ -15,6 +15,8 @@ return [
 
     'name' => env('APP_NAME', 'RMT'),
 
+    'asset_url' => env('ASSET_URL', route('index').'/public/'),
+
     /*
     |--------------------------------------------------------------------------
     | Application Environment
