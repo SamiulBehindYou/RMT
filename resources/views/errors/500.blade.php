6eb1783de4ac73195753819e2cf208036a2c115f
samiulbehindyou <shamiul982@gmail.com>
2025-05-20 12:23:31 +0600

    update error page title

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>NobleUI Responsive Bootstrap 4 Dashboard Template</title>
+	<title>{{ config('app.name') }}</title>
 	<!-- core:css -->
 	<link rel="stylesheet" href="{{ asset('admin') }}/vendors/core/core.css">
 	<!-- endinject -->
